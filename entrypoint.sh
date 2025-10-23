9f237e3242b7ff42f09b3289fdcf57d2f61a74bc
MJavoso <marcoapdr15@gmail.com>
2025-10-23 17:50:18 -0600

    Agregando fixture de lugares de referencia temporal

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,6 +22,7 @@ function all_fixtures() {
     python manage.py loaddata tiposDenuncia
     python manage.py loaddata tiposLugarReferencia
     python manage.py loaddata programasAcademicos
+    python manage.py loaddata lugaresReferencia
 }
 
 # Procesar las banderas pasadas al script
