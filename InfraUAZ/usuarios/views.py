2964dcae621924bfc88461cfa6344d5c9b8aad29
MJavoso <marcoapdr15@gmail.com>
2025-10-23 15:51:49 -0600

    Creando vista para aceptar la petición de crear denunciante

--- InfraUAZ/usuarios/views.py
+++ InfraUAZ/usuarios/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from .forms import DenuncianteForm
 
-# Create your views here.
+def registrar_denunciante(request: HttpRequest):
+    form = DenuncianteForm()
+    creado = False
+    if request.method == 'POST':
+        form = DenuncianteForm(request.POST)
+        if form.is_valid():
+            denunciante = form.save()
+            creado = True
+            # TODO: Enviar correo de verificación
+    context = {
+        'denunciante_form': form,
+        'creado': creado
+    }
+    return render(request, 'crear_cuenta_denunciante.html', context=context)
