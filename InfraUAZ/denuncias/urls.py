8fbb978a637674bf2acdc80afca258e07e54d38c
DanielMtz4 <mynameisdamc@gmail.com>
2025-10-24 17:29:52 -0600

    Agregar los detalles de la denuncia, cambios en la vista y URL's de la denuncias y modificaciones a la sección de muro de denuncias del documento muro_denuncia.html

--- InfraUAZ/denuncias/urls.py
+++ InfraUAZ/denuncias/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.muro_denuncias, name='muro_denuncias'),
-    path('crear-denuncia', views.crear_denuncia, name='crear_denuncia')
+    path('crear-denuncia', views.crear_denuncia, name='crear_denuncia'),
+    # El <int:id_denuncia> hace que la url muestre el id de la denuncia 
+    # despues del nombre de la ruta, por ejemplo: /detalle-denuncia/5/--> 5 es el id de la denuncia
+    path('detalle-denuncia/<int:id_denuncia>/', views.detalle_denuncia, name='detalle_denuncia'),
+    path('denuncia/<int:id_denuncia>/agregar-insumo/', views.agregar_insumo, name='agregar_insumo'),
+
 ]
--- InfraUAZ/denuncias/views.py
+++ InfraUAZ/denuncias/views.py
@@ -1,10 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Denuncia, Insumo
 
-# Create your views here.
-
-
+#------------------ Vista del muro de denuncias ------------------#
 def muro_denuncias(request):
-    return render(request, 'muro_denuncias.html')
+    # Obtener todas las denuncias con sus relaciones necesarias para evitar consultas adicionales
+    denuncias = Denuncia.objects.select_related(
+        # Incluir las relaciones necesarias para optimizar las consultas
+        'id_estado', 
+        'id_lugar__id_programa__id_edificio', 
+        'id_tipo_denuncia'
+    ).order_by('-fecha') # Ordenar por fecha descendente
+    # Renderizar la plantilla con las denuncias obtenidas
+    return render(request, 'muro_denuncias.html', {'denuncias': denuncias})
+
+#------------------ Vista para crear una nueva denuncia ------------------#
 
 def crear_denuncia(request):
     return render(request, 'crear_denuncia.html')
+
+#------------------ Vista de detalle de denuncia ------------------#
+def detalle_denuncia(request, id_denuncia):
+    # Obtener la denuncia por su ID o devolver un error 404 si no existe
+    denuncia = get_object_or_404(Denuncia, id_denuncia=id_denuncia)
+    # Renderizar la plantilla con la denuncia obtenida
+    return render(request, 'detalle_denuncia.html', {'denuncia':denuncia})
+
+# Agrega un insumo en la plantilla detalle_denuncia.html
+# en la parte de insumos
+def agregar_insumo(request, id_denuncia):
+    # Obtener la denuncia por su ID o devolver un error 404 si no existe
+    denuncia = get_object_or_404(Denuncia, pk=id_denuncia)
+    # Si el método de la solicitud es POST, procesa el formulario
+    if request.method == "POST":
+        # Datos del insumo desde el formulario
+        nombre = request.POST.get("nombre")
+        cantidad = request.POST.get("cantidad")
+        costo = request.POST.get("costo")
+
+        # Crear y guardar el nuevo insumo asociado a la denuncia
+        Insumo.objects.create(
+            nombre=nombre,
+            cantidad=cantidad,
+            costo=costo,
+            id_denuncia=denuncia
+        )
+    # Redirigir a la página de detalle de la denuncia después de agregar el insumo
+    return redirect('detalle_denuncia', id_denuncia=id_denuncia)
