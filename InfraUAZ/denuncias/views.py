a1b83d79e2b1f88c88f1fb95e73648f3edb86036
lalosan2323 <eduarsan822@gmail.com>
2025-10-25 12:33:57 -0600

    corrigiendo errores de merge

--- InfraUAZ/denuncias/views.py
+++ InfraUAZ/denuncias/views.py
@@ -1,12 +1,9 @@
+from django.shortcuts import render, get_object_or_404, redirect
 from django.shortcuts import render
 from .forms import DenunciaForm
-from .models import TipoDenuncia, Edificio, TipoLugarReferencia, ProgramaAcademico, LugarReferencia
+from .models import ProgramaAcademico, LugarReferencia, Denuncia, Insumo
 from django.http import JsonResponse
 from django.http import HttpRequest
-# Create your views here.
-
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Denuncia, Insumo
 
 #------------------ Vista del muro de denuncias ------------------#
 def muro_denuncias(request):
@@ -20,33 +17,6 @@ def muro_denuncias(request):
     # Renderizar la plantilla con las denuncias obtenidas
     return render(request, 'muro_denuncias.html', {'denuncias': denuncias})
 
-#------------------ Vista para crear una nueva denuncia ------------------#
-
-def crear_denuncia(request: HttpRequest):
-    denuncia_form = DenunciaForm()
-    creado = False
-    # if request.method == 'POST':
-    #     denuncia_form = DenunciaForm(request.POST)
-    #     if denuncia_form.is_valid():
-    #         denuncia = denuncia_form.save()
-    #         creado = True
-    context = {'denuncia_form':denuncia_form, 'creado':creado}
-    return render(request, 'crear_denuncia.html',context)
-
-def filtrar_programas_por_edificio(request, id_edificio):
-    programas = ProgramaAcademico.objects.filter(id_edificio_id=id_edificio)
-    data = [{"id": p.id_programa, "nombre": p.nombre_programa} for p in programas]
-    return JsonResponse(data, safe=False)
-
-def lugar_referencia(request, id_programa, id_tipolugar):
-    lugares_referencia = LugarReferencia.objects.filter(
-        id_programa_id=id_programa,
-        id_tipo_id=id_tipolugar
-    )
-    data = [{"id": lr.id_lugar, "nombre": lr.nombre_lugar} for lr in lugares_referencia]
-    return JsonResponse(data, safe=False)
-def crear_denuncia(request):
-    return render(request, 'crear_denuncia.html')
 
 #------------------ Vista de detalle de denuncia ------------------#
 def detalle_denuncia(request, id_denuncia):
@@ -76,3 +46,31 @@ def agregar_insumo(request, id_denuncia):
         )
     # Redirigir a la página de detalle de la denuncia después de agregar el insumo
     return redirect('detalle_denuncia', id_denuncia=id_denuncia)
+
+
+
+def crear_denuncia(request: HttpRequest):
+    denuncia_form = DenunciaForm()
+    creado = False
+    # if request.method == 'POST':
+    #     denuncia_form = DenunciaForm(request.POST)
+    #     if denuncia_form.is_valid():
+    #         denuncia = denuncia_form.save()
+    #         creado = True
+    context = {'denuncia_form':denuncia_form, 'creado':creado}
+    return render(request, 'crear_denuncia.html',context)
+
+
+def filtrar_programas_por_edificio(request, id_edificio):
+    programas = ProgramaAcademico.objects.filter(id_edificio_id=id_edificio)
+    data = [{"id": p.id_programa, "nombre": p.nombre_programa} for p in programas]
+    return JsonResponse(data, safe=False)
+
+def lugar_referencia(request, id_programa, id_tipolugar):
+    lugares_referencia = LugarReferencia.objects.filter(
+        id_programa_id=id_programa,
+        id_tipo_id=id_tipolugar
+    )
+    data = [{"id": lr.id_lugar, "nombre": lr.nombre_lugar} for lr in lugares_referencia]
+    return JsonResponse(data, safe=False)
+
