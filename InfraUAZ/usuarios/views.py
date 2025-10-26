f9e53f31380d67ece1ae5cd024298716e1322c46
MJavoso <marcoapdr15@gmail.com>
2025-10-25 20:22:23 -0600

    Creando rutas y vistas de acceso a los templates de login

--- InfraUAZ/usuarios/views.py
+++ InfraUAZ/usuarios/views.py
@@ -3,6 +3,7 @@ from django.http import HttpRequest
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from .forms import DenuncianteForm, LoginForm
 from .models import Usuario
+from .utils import es_administrador
 
 def registrar_denunciante(request: HttpRequest):
     form = DenuncianteForm()
@@ -20,6 +21,9 @@ def registrar_denunciante(request: HttpRequest):
     return render(request, 'crear_cuenta_denunciante.html', context=context)
 
 def login(request: HttpRequest):
+    return render(request, 'login.html')
+
+def login_denunciante(request: HttpRequest):
     form = LoginForm()
     error = None
     if request.method == 'POST':
@@ -40,7 +44,32 @@ def login(request: HttpRequest):
         "form": form,
         "error": error
     }
-    return render(request, 'login.html', context=context)
+    return render(request, 'login_denunciante.html', context=context)
+
+
+def login_administrador(request: HttpRequest):
+    form = LoginForm()
+    error = None
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            correo = form.cleaned_data["correo"]
+            contrasena = form.cleaned_data["contrasena"]
+            try:
+                usuario = Usuario.objects.get(correo=correo)
+                if usuario.check_password(contrasena) and es_administrador(usuario.id):
+                    auth_login(request, usuario)
+                    return redirect('muro_denuncias')
+                else:
+                    error = "Correo o contraseña incorrectos"
+            except Usuario.DoesNotExist:
+                error = "Correo o contraseña incorrectos"
+    context = {
+        "form": form,
+        "error": error
+    }
+    return render(request, 'login_administrador.html', context=context)
+
 
 def logout(request):
     auth_logout(request)
