053ed3a441f9529977c0b07852b29725c531689c
DanielMtz4 <mynameisdamc@gmail.com>
2025-10-27 17:25:45 -0600

    Modificación de la lógica número de denuncias totales, corrección de conflictos en url y views de denuncias

--- InfraUAZ/administrador/views.py
+++ InfraUAZ/administrador/views.py
@@ -1,5 +1,25 @@
-# InfraUAZ/administrador/views.py
 from django.shortcuts import render
+from denuncias.models import Denuncia, EstadoDenuncia
 
+# Vista para el panel de administrador
 def panel_administrador(request):
-    return render(request, 'administrador/panel.html')
+    # Obtener todas las denuncias y sus estados
+    denuncias = Denuncia.objects.select_related('id_estado').all()
+
+    # Calcular total de denuncias y el total de denuncias por estado
+    total = denuncias.count()
+    pendientes = denuncias.filter(id_estado__estado='Pendiente').count()
+    en_revision = denuncias.filter(id_estado__estado='En revisión').count()
+    resueltas = denuncias.filter(id_estado__estado='Resuelta').count()
+    canceladas = denuncias.filter(id_estado__estado='Cancelada').count()
+    # Preparar el contexto para el panel
+    contexto = {
+        'denuncias': denuncias,
+        'total': total,
+        'pendientes': pendientes,
+        'en_revision': en_revision,
+        'resueltas': resueltas,
+        'canceladas': canceladas,
+    }
+    # Renderizar el template del panel de administrador con el contexto
+    return render(request, 'administrador/panel.html', contexto)
