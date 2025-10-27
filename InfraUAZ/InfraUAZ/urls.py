0a5c7427c24c51562a2384104f71f8b0aa247e74
DanielMtz4 <mynameisdamc@gmail.com>
2025-10-27 15:45:01 -0600

    Creación del nuevo módulo 'administrador' y el template de panel de administrador

--- InfraUAZ/InfraUAZ/urls.py
+++ InfraUAZ/InfraUAZ/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('usuarios.urls')),
     path('', include('denuncias.urls')),
+    path('administrador/', include('administrador.urls')), 
 ]
