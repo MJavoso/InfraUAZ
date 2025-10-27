d7466a343f0fd690944f96729ca0085920112602
mime <karymezapmen@gmail.com>
2025-10-27 16:32:59 -0600

    Corrigiendo errores de merge.

--- InfraUAZ/usuarios/urls.py
+++ InfraUAZ/usuarios/urls.py
@@ -13,5 +13,4 @@ urlpatterns = [
     path('login/administrador', views.login_administrador, name='login_administrador'),
     path('logout', views.logout, name='logout'),
     path('registro-admin/', views.registro_admin, name='registro_admin')
-   path('registro-admin/', views.registro_admin, name='registro_admin')
 ]
