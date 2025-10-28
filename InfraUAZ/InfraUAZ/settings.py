fe53e1762f70941223115c6e99bdefe25a32787d
MJavoso <marcoapdr15@gmail.com>
2025-10-28 01:05:51 -0600

    Agregando configuración de backend de servidor SMTP

--- InfraUAZ/InfraUAZ/settings.py
+++ InfraUAZ/InfraUAZ/settings.py
@@ -140,6 +140,13 @@ FIXTURE_DIRS = [
     path.join(BASE_DIR / 'fixtures')
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = getenv('SMTP_EMAIL')
+EMAIL_HOST_PASSWORD = getenv('SMTP_PASSWORD')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
