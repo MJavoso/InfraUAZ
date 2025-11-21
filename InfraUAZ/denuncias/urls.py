2e41cc876064bc2582413c2a0fdcddc6eb799bbf
lalosan2323 <eduarsan822@gmail.com>
2025-11-20 20:30:10 -0600

    Se agrega la funcionalidad Cron, para desactivar cuenta de denunciante al reportar la denuncia.

--- InfraUAZ/denuncias/urls.py
+++ InfraUAZ/denuncias/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('programas/<int:id_edificio>/', views.filtrar_programas_por_edificio, name='filtrar_programas_por_edificio'),
     path('lugar_referencia/<int:id_programa>/<int:id_tipolugar>/', views.lugar_referencia, name='lugar_referencia'),
     path('denuncia/', views.modal_denuncia, name='modal_denuncia'),
+    path('reportar-denuncia', views.reportar_denuncia, name='reportar_denuncia'),
 ]
