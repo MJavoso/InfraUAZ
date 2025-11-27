19f1e0a6ea4b5e416de8a3da5742a1d13d7bf439
DanielMtz4 <mynameisdamc@gmail.com>
2025-11-27 13:29:28 -0600

    Eliminación del perfil de administrador del archivo urls de administrador

--- InfraUAZ/administrador/urls.py
+++ InfraUAZ/administrador/urls.py
@@ -7,8 +7,6 @@ urlpatterns = [
     path('', ListView.as_view(), name='muro_denuncia'),
     # Ruta para la vista del panel de administrador
     path('panel-administrador/', views.panel_administrador, name='panel_administrador'),
-    # Ruta para la vista del perfil del administrador
-    path('perfil/', perfil, name='perfil'),
     # Ruta para filtrar denuncias del panel de denuncias
     path("administrador/filtrar-denuncias/", views.filtrar_denuncias_panel, name="filtrar_denuncias_panel"),
 ]
